5f37eea9d0548a963bf8379a75bb39c0450cab97
Bruno <bruno.buzetti.dev@gmail.com>
2020-11-03 11:51:03 +0100

    nivel 2 parte 2 aula 2

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,9 @@
 import express, { request, response } from 'express';
-import routes from './routes';
+import routes from './routes/index';
 
 const app = express();
 
-app.get('/', (request, response) => {
-
-return response.json({message: "Hello World"});
-
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!')
